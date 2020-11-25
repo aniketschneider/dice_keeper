91a32b90cdcac9e535bea8cc8a697dac59528ff1
Aniket Schneider <1544971+aniketschneider@users.noreply.github.com>
2020-11-24 20:05:29 -0500

    Handle minus signs

--- dice_roller.py
+++ dice_roller.py
@@ -1,34 +1,57 @@
 import random
 import logging
+import re
 
 logging.basicConfig()
 LOG = logging.getLogger(__name__)
 LOG.setLevel(logging.INFO)
 
+TOKEN_PATTERN = '\d*d\d+|\d+|[+-]'
+TOKEN_RE = re.compile(TOKEN_PATTERN)
+
 class DiceRoller():
   def __init__(self):
     random.seed()
 
-  def roll_d(self, size):
+  def _roll_d(self, size):
     return random.randint(1,int(size))
 
+  def _d_exp_value(self, d_exp):
+    factors = d_exp.split('d')
+
+    if len(factors) == 1:
+      # no 'd' means a constant value
+      exp_value = int(factors[0])
+    elif len(factors) == 2:
+      ndice, size = factors
+      if ndice == '':
+        # 'dN' without a preceding numeric factor means 1 die
+        ndice = 1
+      exp_value = sum(self._roll_d(size) for _ in range(int(ndice)))
+    else:
+      # only 1 'd' is allowed
+      raise Exception(f'Malformed dice expression "{d_exp}"')
+
+    return exp_value
+
+
   def roll_string(self, raw_string):
-    LOG.debug(raw_string)
-    dice_string = "".join(raw_string.split())
-    terms = dice_string.split('+')
+    LOG.debug(f"Processing {raw_string}")
+
     result = 0
-    for term in terms:
-      factors = term.split('d')
-      LOG.debug(factors)
-      if len(factors) == 1:
-        result += int(factors[0])
-      elif len(factors) == 2:
-        multiplier, die_size = factors
-        if len(multiplier) == 0:
-          multiplier = 1
-        for _ in range(int(multiplier)):
-          result += self.roll_d(die_size)
-    LOG.info(f'Rolled {dice_string} and got {result}')
+    # whether we should negate the next value
+    sign = 1
+    for token in TOKEN_RE.findall(raw_string):
+      if token == '-':
+        sign = -sign
+      elif token == '+':
+        pass
+      else:
+        term_value = self._d_exp_value(token)
+        result += sign * term_value
+        LOG.debug(f'{token}: {sign * term_value}')
+        sign = 1
+
     return result
 
   def roll_move(self, mod):
@@ -38,3 +61,4 @@ if __name__ == '__main__':
   roller = DiceRoller()
 
   print(roller.roll_string("2d6 + 100"))
+  print(roller.roll_string("2d6 - 7 + d8"))
